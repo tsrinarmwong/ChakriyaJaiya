528df4ac69c6b74320f20185846f86c00f44811b
Timo Srinarmwong <125150205+tsrinarmwong@users.noreply.github.com>
2024-09-13 17:17:38 -0400

    Initial commit with home page slider and styles

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChakriyaJaiya.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Additional logic can go here.
+        }
+    }
+}
---
+++ Pages/Gallery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChakriyaJaiya.Pages
+{
+    public class GalleryModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Logic to fetch and display photos will go here.
+        }
+    }
+}
